4deb66a08526c94c4257e1ac71c31b2105e98c2a
Akanksha Dixit <akanksha@Akankshas-MacBook-Air.local>
2021-02-18 16:49:33 +0530

    Added Asset transfer basic chaincode
    - places asset transfer basic code
    - made changes to createChannel.sh file for Org3
    - removed bin folder

--- networks/single-node/scripts/createChannel.sh
+++ networks/single-node/scripts/createChannel.sh
@@ -23,7 +23,7 @@ createChannelGenesisBlock() {
 		fatalln "configtxgen tool not found."
 	fi
 	set -x
-	configtxgen -profile TwoOrgsApplicationGenesis -outputBlock ./channel-artifacts/${CHANNEL_NAME}.block -channelID $CHANNEL_NAME
+	configtxgen -profile ThreeOrgsApplicationGenesis -outputBlock ./channel-artifacts/${CHANNEL_NAME}.block -channelID $CHANNEL_NAME
 	res=$?
 	{ set +x; } 2>/dev/null
   verifyResult $res "Failed to generate channel configuration transaction..."
@@ -49,7 +49,7 @@ createChannel() {
 
 # joinChannel ORG
 joinChannel() {
-  FABRIC_CFG_PATH=$PWD/../config/
+  FABRIC_CFG_PATH=$PWD/config/
   ORG=$1
   setGlobals $ORG
 	local rc=1
@@ -79,7 +79,7 @@ FABRIC_CFG_PATH=${PWD}/configtx
 infoln "Generating channel genesis block '${CHANNEL_NAME}.block'"
 createChannelGenesisBlock
 
-FABRIC_CFG_PATH=$PWD/../config/
+FABRIC_CFG_PATH=$PWD/config/
 BLOCKFILE="./channel-artifacts/${CHANNEL_NAME}.block"
 
 ## Create channel
@@ -92,11 +92,15 @@ infoln "Joining org1 peer to the channel..."
 joinChannel 1
 infoln "Joining org2 peer to the channel..."
 joinChannel 2
+infoln "Joining org3 peer to the channel..."
+joinChannel 3
 
 ## Set the anchor peers for each org in the channel
 infoln "Setting anchor peer for org1..."
 setAnchorPeer 1
 infoln "Setting anchor peer for org2..."
 setAnchorPeer 2
+infoln "Setting anchor peer for org3..."
+setAnchorPeer 3
 
 successln "Channel '$CHANNEL_NAME' joined"
